6f31d1b25949683986d147c44bc2331d6adfe160
cdx <cdx@l7w.org>
2025-02-14 20:30:14 -0800

    updated degrees

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,7 +1,12 @@
 const degrees = [
   {
+    school: 'Self-Taught::Age 10+',
+    degree: 'Linux, C, and Visual Basic',
+    year: '1991 -- Ongoing',
+  },
+  {
     school: 'City College of SF',
-    degree: 'Computer Sciences Core - Java and C++',
+    degree: 'Computer Sciences Core - Java, C++ & BASH',
     year: '2011 -- 2014',
   },
   {
